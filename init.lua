b42a775bbbe675f044a1f7df6142d79fb795fea2
Wulgaren <natios100@gmail.com>
2026-05-14 15:34:53 +0200

    windows fix

--- init.lua
+++ init.lua
@@ -1,6 +1,15 @@
 vim.g.mapleader = ' '
 vim.env.NVIM_LOG_FILE = vim.fn.stdpath('state') .. '/nvim.log'
 
+if vim.uv.os_uname().sysname == 'Windows_NT' and vim.fn.executable('cl') ~= 1 then
+  if vim.fn.executable('gcc') == 1 then
+    vim.env.CC = vim.env.CC or 'gcc'
+  end
+  if vim.fn.executable('g++') == 1 then
+    vim.env.CXX = vim.env.CXX or 'g++'
+  end
+end
+
 local data_path = vim.fn.stdpath('data')
 local path_sep = package.config:sub(1, 1)
 local lazypath = table.concat({ data_path, 'lazy', 'lazy.nvim' }, path_sep)
