e5601e7de5b3dbc35bf95ed8e54be72016d2ce33
Wulgaren <natios100@gmail.com>
2026-08-08 16:47:55 +0200

    changes

--- init.lua
+++ init.lua
@@ -1,23 +1,6 @@
 vim.g.mapleader = " "
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.runtimepath:prepend(lazypath)
-
-require("lazy").setup(require("plugins"), {
-  checker = { enabled = false },
-  rocks = { enabled = false },
-})
-
+require("plugins")
 require("options")
 require("lsp")
 require("colorscheme")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,47 +1,36 @@
-return {
-  {
-    'mason-org/mason.nvim',
-    lazy = false,
-    cmd = { 'Mason', 'MasonInstall', 'MasonUpdate', 'MasonLog' },
-    opts = {
-      registries = {
-        'github:mason-org/mason-registry',
-        'github:Crashdummyy/mason-registry',
-      },
-    },
-    config = function(_, opts)
-      require('mason').setup(opts)
-      local mason_bin = vim.fn.stdpath('data') .. '/mason/bin'
-      vim.env.PATH = mason_bin .. ':' .. vim.env.PATH
-    end,
+vim.pack.add({
+  'https://github.com/mason-org/mason.nvim',
+  'https://github.com/monkoose/neocodeium',
+  'https://github.com/tpope/vim-fugitive',
+}, { confirm = false, load = true })
+
+require('mason').setup({
+  registries = {
+    'github:mason-org/mason-registry',
+    'github:Crashdummyy/mason-registry',
   },
-  {
-    'monkoose/neocodeium',
-    event = 'VeryLazy',
-    opts = {
-      filetypes = {
-        TelescopePrompt = false,
-      },
-    },
-    config = function(_, opts)
-      local neocodeium = require('neocodeium')
-      neocodeium.setup(opts)
-      vim.keymap.set('n', '<leader>ko', '<cmd>NeoCodeium toggle<CR>', {
-        silent = true,
-        desc = 'NeoCodeium toggle (no bang; use :NeoCodeium! toggle to halt server)',
-      })
-      --- Full suggestion accept (`accept`, not word). `<M-Tab>` = Alt+Tab; OS may capture it before Neovim.
-      vim.keymap.set('i', '<M-Tab>', neocodeium.accept, { silent = true, desc = 'NeoCodeium: accept all' })
-      vim.keymap.set('i', '<M-w>', neocodeium.accept_word, { silent = true, desc = 'NeoCodeium: accept word' })
-      vim.keymap.set('i', '<M-l>', neocodeium.accept_line, { silent = true, desc = 'NeoCodeium: accept line' })
-      vim.keymap.set('i', '<Tab>', function()
-        if neocodeium.visible() then
-          neocodeium.accept_word()
-        else
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', true)
-        end
-      end, { silent = true, desc = 'NeoCodeium: accept word, else insert Tab' })
-    end,
+})
+local mason_bin = vim.fn.stdpath('data') .. '/mason/bin'
+vim.env.PATH = mason_bin .. ':' .. vim.env.PATH
+
+local neocodeium = require('neocodeium')
+neocodeium.setup({
+  filetypes = {
+    TelescopePrompt = false,
   },
-  { 'tpope/vim-fugitive' },
-}
+})
+vim.keymap.set('n', '<leader>ko', '<cmd>NeoCodeium toggle<CR>', {
+  silent = true,
+  desc = 'NeoCodeium toggle (no bang; use :NeoCodeium! toggle to halt server)',
+})
+--- Full suggestion accept (`accept`, not word). `<M-Tab>` = Alt+Tab; OS may capture it before Neovim.
+vim.keymap.set('i', '<M-Tab>', neocodeium.accept, { silent = true, desc = 'NeoCodeium: accept all' })
+vim.keymap.set('i', '<M-w>', neocodeium.accept_word, { silent = true, desc = 'NeoCodeium: accept word' })
+vim.keymap.set('i', '<M-l>', neocodeium.accept_line, { silent = true, desc = 'NeoCodeium: accept line' })
+vim.keymap.set('i', '<Tab>', function()
+  if neocodeium.visible() then
+    neocodeium.accept_word()
+  else
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', true)
+  end
+end, { silent = true, desc = 'NeoCodeium: accept word, else insert Tab' })
